18afddea500f7eff838f314b510136d320806468
anvo <anvo@uio.no>
2026-07-15 15:11:11 +0200

    Add RecipeType enum

--- KnitApp/Models/RecipeType.cs
+++ KnitApp/Models/RecipeType.cs
@@ -2,5 +2,7 @@ namespace KnitApp.Models;
 
 public enum RecipeType
 {
-    
+    Knit, 
+    Crochet
+    //unsure if it should be added "combination" because there are recipes that use both but mainly knit + a bit crochet
 }
